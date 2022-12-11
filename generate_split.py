f3171cbf4aad0ad88c15af2ae9414c2b452ff090
Oleksandr <lantainzp@gmail.com>
2022-12-11 21:39:46 +0200

    fix

--- generate_split.py
+++ generate_split.py
@@ -5,24 +5,24 @@ import os
 from src.util import get_n_files_from, remove_files_from_dir
 # from processors.csv import transfer_n_fields
 
-DS_OUT = f"./out/ds"
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Transform folder into protoc dataset',
         formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('--source')
+    parser.add_argument('--out')
     args = parser.parse_args()
     try:
-        remove_files_from_dir(DS_OUT)
+        remove_files_from_dir(args.out)
 
-        os.mkdir(f"{DS_OUT}")
-        os.mkdir(f"{DS_OUT}/images")
-        os.mkdir(f"{DS_OUT}/images/Bee")
+        os.mkdir(f"{args.out}")
+        os.mkdir(f"{args.out}/images")
+        os.mkdir(f"{args.out}/images/Bee")
     except:
         print("Whatever")
         
     files = get_n_files_from("./out/datasets/remo/crop/Bee", 1000)
     for file in files:
-        shutil.copyfile(f"./out/datasets/remo/crop/Bee/{file}", f"{DS_OUT}/images/Bee/{file}")
+        shutil.copyfile(f"./out/datasets/remo/crop/Bee/{file}", f"{args.out}/images/Bee/{file}")
     
